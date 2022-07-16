3f95d1aeb9609938a6ceda359854cf5a10d27cb2
Ryan Gabriel <ryanb@pop-os.localdomain>
2022-07-16 20:05:40 -0300

    feat: second-step-finished(js)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
-function rockPaperScissors() {
-  let rock = document.getElementById('#rock')
-  let paper = document.getElementById('paper')
-  let scissors = document.getElementById('scissors')
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+
+const userScore = 0
+const SistemScore = 0
+
+const hands = ['handRock', 'handPaper', 'handScissors']
+
+let handUser = ''
+let handSistem = ''
+
+rock.addEventListener('click', function () {
+  handUser = hands[0]
+})
+paper.addEventListener('click', function () {
+  handUser = hands[1]
+})
+scissors.addEventListener('click', function () {
+  handUser = hands[2]
+})
+
+
+function getSistemChoice() {
+  const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min)
+  handSistem = hands[randomNumber(0, hands.length)]
 }
+
+getSistemChoice()
+
+function main() {
+  // let numberAux = (min, max) => Math.floor(Math.random() * (max - min) + min)
+}
+
+window.alert(`${handSistem}`)
