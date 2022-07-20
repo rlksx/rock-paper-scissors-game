10172d56c07b9b558efe43a02ea4918e023f87f1
Ryan Gabriel <ryanb@pop-os.localdomain>
2022-07-20 16:50:21 -0300

    feat: bug-fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,75 @@ const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 
 const userScore = 0
-const SistemScore = 0
+const SystemScore = 0
 
-const hands = ['handRock', 'handPaper', 'handScissors']
+const hands = ['hr', 'hp', 'hs']
+/* hr == hand rock, hp == hand paper, hs == hand scissors */
 
 let handUser = ''
-let handSistem = ''
+let handSystem = ''
 
 rock.addEventListener('click', function () {
   handUser = hands[0]
+  getSystemChoice()
+  main()
 })
 paper.addEventListener('click', function () {
   handUser = hands[1]
+  getSystemChoice()
+  main()
 })
 scissors.addEventListener('click', function () {
   handUser = hands[2]
+  getSystemChoice()
+  main()
 })
 
-
-function getSistemChoice() {
-  const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min)
-  handSistem = hands[randomNumber(0, hands.length)]
+function getSystemChoice() {
+  const randomNumber = (min, max) =>
+    Math.floor(Math.random() * (max - min) + min)
+  handSystem = hands[randomNumber(0, hands.length)]
 }
-
-getSistemChoice()
 
 function main() {
-  // let numberAux = (min, max) => Math.floor(Math.random() * (max - min) + min)
+  switch (handUser + handSystem) {
+    case 'hrhr':
+      playDraw()
+      break
+    case 'hrhp':
+      playLosed()
+      break
+    case 'hrhs':
+      playWin()
+      break
+    case 'hphr':
+      playWin()
+      break
+    case 'hphp':
+      playDraw()
+      break
+    case 'hphs':
+      playLosed()
+      break
+    case 'hshr':
+      playLosed()
+      break
+    case 'hshp':
+      playWin()
+      break
+    case 'hshs':
+      playDraw()
+      break
+  }
+  // window.alert(`${handUser + handSystem}`)
 }
 
-window.alert(`${handSistem}`)
+function playWin() {
+  window.alert('você ganhou parabéns')
+}
+function playLosed() {
+  window.alert('voce perdeu')
+}
+function playDraw() {
+  window.alert('você empatou com o sistema')
+}
